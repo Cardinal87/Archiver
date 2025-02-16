a91f01765ce43d34175102d749e09f3d23bcaab0
Cardinal87 <artem.cheglakov.234@gmail.com>
2025-02-16 12:51:42 +0300

    add: extracting text from images and saving as pdf

--- Archiver.API/Controllers/ConverterController.cs
+++ Archiver.API/Controllers/ConverterController.cs
@@ -1,9 +1,19 @@
 ﻿using Archiver.API.DTO;
+using PuppeteerSharp;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using PuppeteerSharp;
-using System.IO.Compression;    
+using System.IO.Compression;
+using Tesseract;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Kernel.Pdf.Event;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using static System.Net.Mime.MediaTypeNames;
+
+
 
 namespace Archiver.API.Controllers
 {
@@ -12,6 +22,7 @@ namespace Archiver.API.Controllers
     [ApiController]
     public class ConverterController : Controller
     {
+        private static string tessDataPath = "tessdata";
         private static string sourceFiles = "files";
         private static string outputFile = "files.zip";
         private OutputOptions _options;
@@ -22,7 +33,8 @@ namespace Archiver.API.Controllers
         }
         
         [HttpPost("parse")]
-        public async Task<IActionResult> ParseRawData([FromBody] RequestModel model)
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> ParseRawData([FromForm] RequestModel model)
         {
             try
             {
@@ -30,13 +42,25 @@ namespace Archiver.API.Controllers
                 {
                     Directory.CreateDirectory(sourceFiles);
                 }
+                int pdfNum = 1;
                 if (model.HtmlUrls.Count != 0)
                 {
-                    int pdfNum = 1;
                     foreach (var url in model.HtmlUrls)
                     {
                         await ParseHtmlToPdf(url, pdfNum);
                         pdfNum++;
+                    }
+                     
+                }
+                if (model.Pictures != null)
+                {
+                    foreach (var image in model.Pictures)
+                    {
+                        if (image.Length > 0)
+                        {
+                            await HandleImages(image, pdfNum);
+                            pdfNum++;
+                        }
                     }
                 }
                 string outputSource = Path.Combine(_options.outputDir, outputFile);
@@ -49,6 +73,33 @@ namespace Archiver.API.Controllers
             }
         }
 
+        private async Task HandleImages(IFormFile image, int docnum)
+        {
+            using var engine = new TesseractEngine("tessdata", "rus+eng", EngineMode.LstmOnly);
+            var memoryStream = new MemoryStream();
+            await image.CopyToAsync(memoryStream);
+            var bytes = memoryStream.ToArray();
+            var pix = Pix.LoadFromMemory(bytes);
+            using var page = engine.Process(pix);
+            var text = page.GetText();
+            ParseTextToPdf(text, docnum);
+        }
+
+
+        private void ParseTextToPdf(string text, int pdfNum)
+        {
+            var outPath = Path.Combine(sourceFiles, $"pdf{pdfNum}");
+            using var pdfWriter = new PdfWriter(outPath);
+            using var pdf = new PdfDocument(pdfWriter);
+            Document doc = new(pdf, iText.Kernel.Geom.PageSize.A4, false);
+            doc.SetMargins(20, 20, 20, 20);
+            var p = new Paragraph(text)
+                .SetTextAlignment(TextAlignment.LEFT)
+                .SetFontSize(14)
+                .SetFirstLineIndent(50);
+            doc.Add(p);
+            doc.Close();
+        }
 
 
         private async Task ParseHtmlToPdf(string url, int pdfNum)
@@ -57,7 +108,8 @@ namespace Archiver.API.Controllers
             using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
             using var page = await browser.NewPageAsync();
             await page.GoToAsync(url);
-            await page.PdfAsync($"files/{pdfNum}");
+            var outPath = Path.Combine(sourceFiles, $"pdf{pdfNum}");
+            await page.PdfAsync(outPath);
         }
 
     }
--- Archiver.API/DTO/RequestModel.cs
+++ Archiver.API/DTO/RequestModel.cs
@@ -1,10 +1,14 @@
-﻿namespace Archiver.API.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Archiver.API.DTO
 {
     public class RequestModel
     {
+        
         public List<string> HtmlUrls { get; set; } = new List<string>();
 
-
+        [FileExtensions(Extensions = "png, jpeg, tiff, bmp")]
+        public IFormFileCollection? Pictures { get; set; }
 
     }
 }
